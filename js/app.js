6bfde62b6b3a7d463bc9a4cfd3b54902a8e3c45d
justno1 <justno1@hotmail.com.tw>
2014-05-28 13:11:44 +0800

    mac hw3 second commit

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@
     templates[e] = doT.template(dom);
   }); 
 
- 可選-編寫共用函數();
+// 可選-編寫共用函數();
  var handler = {
    var currentUser = Parse.User.current();
 
@@ -21,14 +21,14 @@
 
      if(currentUser){
 
-       顯示哪些button();
+ //      顯示哪些button();
        document.getElementById('loginButton').style.display = "none"; 
        document.getElementById('evaluationButton').style.display = "display"; 
        document.getElementById('logoutButton').style.display = "display"; 
 
      } else {
 
-       顯示哪些button();    
+ //      顯示哪些button();    
        document.getElementById('loginButton').style.display = "display"; 
        document.getElementById('evaluationButton').style.display = "none"; 
        document.getElementById('logoutButton').style.display = "none"; 
@@ -38,7 +38,7 @@
 
    logInViewFunc: function(redirect){
 
-     把版型印到瀏覽器上();
+  //   把版型印到瀏覽器上();
       document.getElementById('content').innerHTML = templates.loginView();
       var currentUser = Parse.User.current();
       var postAction = function(){
@@ -50,22 +50,22 @@
         window.location.hash = '';
       } 
       else{
-     綁定登入表單的學號檢查事件(); // 可以利用TAHelp物件
+ //    綁定登入表單的學號檢查事件(); // 可以利用TAHelp物件
         var messgae = (TAHelp.isValidStudentID(document.getElementById('form-signin-student-id').value)===false ? '學號不存在，請再確認一次' : '');
         document.getElementById('form-signin-messgae').innerHTML = message;
           
-     綁定註冊表單的學號檢查事件(); // 可以利用TAHelp物件
+ //    綁定註冊表單的學號檢查事件(); // 可以利用TAHelp物件
         var messgae = (TAHelp.isValidStudentID(document.getElementById('form-signup-student-id').value)===false ? '學號不存在，請再確認一次' : '');
         document.getElementById('form-signup-messgae').innerHTML = message;
 
-     綁定註冊表單的密碼檢查事件(); // 參考上課範例
+ //    綁定註冊表單的密碼檢查事件(); // 參考上課範例
      document.getElementById('form-signup-password1').value.addEventListener('keyup',function(){
       var signupForm_password = document.getElementById('form-signin-password');
       var messgae = (this.value !== signupForm_password.value) ? '密碼不一致，請再確認一次' : '';
         document.getElementById('form-signin-messgae').innerHTML = messgae;
      })
 
-     綁定登入表單的登入檢查事件(); // 送出還要再檢查一次，這裡會用Parse.User.logIn
+  //   綁定登入表單的登入檢查事件(); // 送出還要再檢查一次，這裡會用Parse.User.logIn
      document.getElementById('form-signin').addEventListener('submit',function(){
       Parse.User.logIn(document.getElementById('form-signin-student-id').value,
       document.getElementById('form-signin-password').value,{
@@ -77,7 +77,7 @@
       });
      });
 
-     綁定註冊表單的註冊檢查事件(); // 送出還要再檢查一次，這裡會用Parse.User.signUp和相關函數
+  //   綁定註冊表單的註冊檢查事件(); // 送出還要再檢查一次，這裡會用Parse.User.signUp和相關函數
      document.getElementById('form-signup').addEventListener('submit',function(){
       var user = new Parse.User();
       user.set("username",document.getElementById('form-signup-student-id').value);
@@ -108,7 +108,7 @@
          parseACL.setPublicWriteAccess(false);
          parseACL.setReadAccess(userCurrent,true);
          parseACL.setWriteAccess(userCurrent,true);
-         
+
      var parseQuery = new Parse.Query(evaluation);
          parseQuery.equalTo("user",userCurrent);
          
@@ -127,13 +127,13 @@
           }
          })
 
-     問看看Parse有沒有這個使用者之前提交過的peer review物件(
+//     問看看Parse有沒有這個使用者之前提交過的peer review物件(
 
-     沒有的話: 從TAHelp生一個出來(加上scores: [‘0’, ‘0’, ‘0’, ‘0’]屬性存分數並把自己排除掉)
+//     沒有的話: 從TAHelp生一個出來(加上scores: [‘0’, ‘0’, ‘0’, ‘0’]屬性存分數並把自己排除掉)
 
-     把peer review物件裡的東西透過版型印到瀏覽器上();
+//     把peer review物件裡的東西透過版型印到瀏覽器上();
 
-     綁定表單送出的事件(); // 如果Parse沒有之前提交過的peer review物件，要自己new一個。或更新分數然後儲存。
+//     綁定表單送出的事件(); // 如果Parse沒有之前提交過的peer review物件，要自己new一個。或更新分數然後儲存。
 
      );
 
